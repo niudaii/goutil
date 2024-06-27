50f51b7b68a3633a7c7ad95e77a50bea395476aa
gaoqianyang <gaoqianyang>
2024-06-27 10:40:49 +0800

    feat(kafka): consumer config

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -25,13 +25,11 @@ type Consumer struct {
 func NewConsumer(config *ConsumerConfig) (consumer *Consumer) {
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
-	saramaConfig.Net.DialTimeout = 60 * time.Second
-	saramaConfig.Net.ReadTimeout = 60 * time.Second
-	saramaConfig.Net.WriteTimeout = 60 * time.Second
-	saramaConfig.Consumer.Group.Rebalance.Retry.Max = 10
+	saramaConfig.Net.DialTimeout = 90 * time.Second
+	saramaConfig.Net.ReadTimeout = 90 * time.Second
+	saramaConfig.Net.WriteTimeout = 90 * time.Second
 	saramaConfig.Consumer.Return.Errors = true
 	saramaConfig.Consumer.Group.Rebalance.Timeout = 3 * time.Minute
-	saramaConfig.Consumer.Group.Session.Timeout = 10 * time.Second
 	if config.Kerberos.KeytabPath != "" {
 		saramaConfig.Net.SASL.Mechanism = sarama.SASLTypeGSSAPI
 		saramaConfig.Net.SASL.GSSAPI.AuthType = sarama.KRB5_KEYTAB_AUTH

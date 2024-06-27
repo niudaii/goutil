5961c904c19b3dba8426000bceb417c26c3418e4
gaoqianyang <gaoqianyang>
2024-06-27 09:13:39 +0800

    feat(httpserver): config

--- httpserver/config.go
+++ httpserver/config.go
@@ -1,7 +1,6 @@
 package httpserver
 
 type Config struct {
-	Enabled bool   `yaml:"enabled" json:"enabled"`
-	Mode    string `yaml:"mode" json:"mode"`
-	Port    string `yaml:"port" json:"port"`
+	Mode string `yaml:"mode" json:"mode"`
+	Port string `yaml:"port" json:"port"`
 }

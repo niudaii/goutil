e6c0c46b5fd19ef74947b12734803be8382cb4d3
zp857 <114202319+zp857@users.noreply.github.com>
2024-07-05 14:09:39 +0800

    refactor(consumer): timeout

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -26,12 +26,12 @@ type Consumer struct {
 func NewConsumer(config *ConsumerConfig) (consumer *Consumer) {
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
-	saramaConfig.Net.DialTimeout = 90 * time.Second
-	saramaConfig.Net.ReadTimeout = 90 * time.Second
-	saramaConfig.Net.WriteTimeout = 90 * time.Second
+	saramaConfig.Net.DialTimeout = 3 * time.Minute
+	saramaConfig.Net.ReadTimeout = 3 * time.Minute
+	saramaConfig.Net.WriteTimeout = 3 * time.Minute
 	saramaConfig.Consumer.Return.Errors = true
-	saramaConfig.Consumer.Group.Session.Timeout = 3 * time.Minute
-	saramaConfig.Consumer.Group.Rebalance.Timeout = 3 * time.Minute
+	saramaConfig.Consumer.Group.Session.Timeout = 5 * time.Minute
+	saramaConfig.Consumer.Group.Rebalance.Timeout = 5 * time.Minute
 	if config.Kerberos.KeytabPath != "" {
 		saramaConfig.Net.SASL.Mechanism = sarama.SASLTypeGSSAPI
 		saramaConfig.Net.SASL.GSSAPI.AuthType = sarama.KRB5_KEYTAB_AUTH

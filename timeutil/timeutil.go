6a61eee908c571ef4b26d9decac50f3c0a64f35c
zp857 <114202319+zp857@users.noreply.github.com>
2024-07-22 16:51:43 +0800

    feat: RandomSleep

--- timeutil/timeutil.go
+++ timeutil/timeutil.go
@@ -1,7 +1,16 @@
 package timeutil
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 func IsBetween(t time.Time, start, end time.Time) bool {
 	return t.Equal(start) || t.After(start) && t.Before(end) || t.Equal(end)
+}
+
+func RandomSleep() {
+	rand.NewSource(time.Now().UTC().UnixNano())
+	n := rand.Intn(10)
+	time.Sleep(time.Duration(n) * time.Second)
 }

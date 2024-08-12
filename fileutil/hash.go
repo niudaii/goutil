56b201e51e0d8b124dd1f84fe9e88c96e2b2f463
zp857 <114202319+zp857@users.noreply.github.com>
2024-08-12 16:33:12 +0800

    feat: 计算文件md5、sha256

---
+++ fileutil/hash.go
@@ -0,0 +1,41 @@
+package fileutil
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+)
+
+// CalculateMD5 计算文件的 MD5 哈希
+func CalculateMD5(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
+
+// CalculateSHA256 计算文件的 SHA256 哈希
+func CalculateSHA256(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}

0642ac07b34954fe846d8c9f058e642ed7dc4332
gqy <gaoqianyang@cn.net.ntes>
2025-01-08 15:31:39 +0800

    update

--- redis/config.go
+++ redis/config.go
@@ -2,12 +2,12 @@ package redis
 
 import "fmt"
 
-type Redis struct {
+type Config struct {
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
 	Password string `yaml:"password"`
 }
 
-func (r *Redis) Addr() string {
-	return fmt.Sprintf("%v:%v", r.Host, r.Port)
+func (c Config) Addr() string {
+	return fmt.Sprintf("%v:%v", c.Host, c.Port)
 }

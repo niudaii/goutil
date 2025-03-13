095dcb666c4877899cb9d8bba4ebcfd83f02534c
gqy <gaoqianyang@cn.net.ntes>
2025-03-13 13:40:06 +0800

    'update'

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -2,8 +2,8 @@ package strutil
 
 import "strings"
 
-func SplitByComma(itemString string) []string {
-	items := strings.Split(itemString, ",")
+func SplitByComma(s string) []string {
+	items := strings.Split(s, ",")
 	for i, item := range items {
 		items[i] = strings.TrimSpace(item)
 	}
@@ -15,4 +15,18 @@ func FirstUpper(s string) string {
 		return ""
 	}
 	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+func RemoveDigits(s string) string {
+	re := regexp.MustCompile("[0-9]+")
+	return re.ReplaceAllString(s, "")
+}
+
+func IsChinese(s string) bool {
+	for _, r := range s {
+		if !unicode.Is(unicode.Han, r) {
+			return false
+		}
+	}
+	return true
 }
